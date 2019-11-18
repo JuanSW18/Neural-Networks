a464d221fe74ffd4668dbbbdab081c5c94ba28f3
Juan Eneque <juan.eneque@unmsm.edu.pe>
2019-11-18 18:57:55 -0500

    mlp

--- mlp/mlp.m
+++ mlp/mlp.m
@@ -2,7 +2,21 @@ function mlp()
     fprintf(1, "------------------------------------- \n");
     fprintf(1, "\x7c                MLP                \x7c\n");
     fprintf(1, "------------------------------------- \n");
-    leerCapas();
+    [P, T] = leerValoresDeArchivos();
+    %[a, b] = leerRango();
+    a = -2;
+    b = 2;
+    %[v1, v2] = leerCapas();
+    v1 = [1 2 3 1];
+    v2 = [2 3 1];
+    %alpha = leerAlpha();
+    alpha = 0.15;
+    %[epochmax, eepoch, epochval, numval] = leerValoresParaCriterios();
+    epochmax = 15;
+    eepoch = 0.001;
+    epochval = 10;
+    numval = 5;
+    iniciarAprendizaje(P, T, alpha, v1, v2, epochmax, epochval);
 end
 
 function [P, T] = leerValoresDeArchivos()
@@ -78,7 +92,7 @@ function F = calcularFs(v1, v2, valores_a)
     % valores_a = {a0=p, a1, a2, a3}
     F = {};    
     nro_capas = size(v1);
-    for i = 2:nro_capas
+    for i = 2:nro_capas(2)
         f = zeros(v1(i), v1(i));
         switch v2(i-1)
             case 1
@@ -88,39 +102,44 @@ function F = calcularFs(v1, v2, valores_a)
                 end
             case 2
                 % logsig
-                 for j = 1:v1(i)
-                    f(j, j) = valores_a{i}(j, 1)*(1 - valores_a{i}(j, 1));
+                aux =  cell2mat( valores_a(1, i) );
+                for j = 1:v1(i)
+                    f(j, j) = aux(j, 1)*(1 - aux(j, 1));
                 end
             case 3
                 % tansig
-                 for j = 1:v1(i)
-                    f(j, j) = 1 - valores_a{i}(j, 1)*valores_a{i}(j, 1);
+                aux =  cell2mat( valores_a(1, i) );
+                for j = 1:v1(i)
+                    f(j, j) = 1 - aux(j, 1)*aux(j, 1);
                 end
-        end   
+        end
+        F{i-1} = f;
     end 
 end
 
 function S = calcularSensitividades(nro_capas, F, e, capas_w_b)
     S = {};
-    for n = 1:nro_capas
+    for n = 1:nro_capas(2)-1
         S{n} = 0;
     end
-    S{nro_capas} = -2*F(nro_capas)*e;
-    for i = nro_capas-1:-1:1
-        S{i} = F(i)*(capas_w_b{i+1}.w)'*S{i+1};
+    S{nro_capas(2)-1} = -2*F{nro_capas(2)-1}*e;
+    for i = nro_capas-2:-1:1
+        S{i} = F{i}*(capas_w_b{i+1}.w)'*S{i+1};
     end
 end
 
-function w = calcularNuevoW(nro_capas, w_old, alpha, S, arreglo_a)
-    for i = 2:nro_capas
-        w = w_old - alpha*S{i}*arreglo_a{i-1};
-    end
+function w = calcularNuevoW(capa, w_old, alpha, S, arreglo_a)
+    w = w_old - alpha*S{capa}*arreglo_a{capa}';
+%     for i = 2:nro_capas
+%         w = w_old - alpha*S{i}*arreglo_a{i-1}';
+%     end
 end
 
-function b = calcularNuevoBias(nro_capas, b_old, alpha, S)
-    for i = 1:nro_capas
-        b = b_old - alpha*S{i};
-    end
+function b = calcularNuevoBias(capa, b_old, alpha, S)
+    b = b_old - alpha*S{capa};
+%     for i = 1:nro_capas
+%         b = b_old - alpha*S{i};
+%     end
 end
 
 function iniciarAprendizaje(P, T, alpha, v1, v2, epochmax, epochval)
@@ -132,11 +151,11 @@ function iniciarAprendizaje(P, T, alpha, v1, v2, epochmax, epochval)
     
     nro_capas = size(v1);
     capas_w_b = {};
-    for j = 1:nro_capas(2)-1
+    for c = 1:nro_capas(2)-1
         objeto = LayerMLP;
-        objeto.w =  -1 + 2*rand( v1(j+1), v1(j) );
-        objeto.b = -1 + 2*rand( v1(j+1), 1 );
-        capas_w_b{j} = objeto;
+        objeto.w =  -1 + 2*rand( v1(c+1), v1(c) );
+        objeto.b = -1 + 2*rand( v1(c+1), 1 );
+        capas_w_b{c} = objeto;
     end
     
     for epoch_actual = 1:epochmax
@@ -150,12 +169,21 @@ function iniciarAprendizaje(P, T, alpha, v1, v2, epochmax, epochval)
                 target = T(nro_p, :)';
                 % CALCULO DE 'a' CAPA POR CAPA
                 arreglo_a{1} = p;
+                fprintf(1, "P %d\n", nro_p);
                 for capa = 1:nro_capas(2)-1
-                    n = capas_w_b{capa}.w * p + capas_w_b{capa}.b;
+%                     fprintf(1, "CAPA %d\n", capa);
+%                     fprintf(1, "W\n");
+%                     disp(capas_w_b{capa}.w);
+%                     fprintf(1, "p\n");
+%                     disp(p);
+%                     fprintf(1, "bias\n");
+%                     disp(capas_w_b{capa}.b);
+                    n = (capas_w_b{capa}.w)*p + capas_w_b{capa}.b;
                     a = calcularA(n, v2(capa));
                     p = [];
                     p = a;
-                    
+%                     fprintf(1, "a\n");
+%                     disp(a);
                     % GUARDAMOS EL VALOR FINAL DE a PARA CADA CAPA
                     arreglo_a{capa+1} = a;
                 end
@@ -166,13 +194,21 @@ function iniciarAprendizaje(P, T, alpha, v1, v2, epochmax, epochval)
                 errores_iteracion{nro_p} = e;
                 
                 % CALCULO DE NUEVOS W y b
-                F = calcularFs(v1, v2, a);
+                F = calcularFs(v1, v2, arreglo_a);
                 S = calcularSensitividades(nro_capas, F, e, capas_w_b);
+%                 fprintf(1, "Nuevos w y b\n");
                 for j = 1:nro_capas(2)-1
                     w_old = capas_w_b{j}.w;
                     b_old = capas_w_b{j}.b;
-                    capas_w_b{j}.w = calcularNuevoW(nro_capas, w_old, alpha, S, arreglo_a);
-                    capas_w_b{j}.b = calcularNuevoBias(nro_capas, b_old, alpha, S);
+                    capas_w_b{j}.w = calcularNuevoW(j, w_old, alpha, S, arreglo_a);
+                    capas_w_b{j}.b = calcularNuevoBias(j, b_old, alpha, S);
+%                     fprintf(1, "j => %d\n", j);
+%                     fprintf(1, "old values\n");
+%                     disp(w_old);
+%                     disp(b_old);
+%                     fprintf(1, "new values\n");
+%                     disp(capas_w_b{j}.w);
+%                     disp(capas_w_b{j}.b);
                 end
                 % fin del CALCULO DE NUEVOS W y b
                 
@@ -188,15 +224,6 @@ function iniciarAprendizaje(P, T, alpha, v1, v2, epochmax, epochval)
         end
     end
 end
-
-% CALCULO DE NUEVOS W y b
-%                 for j = 1:nro_capas(2)-1
-%                     w_old = capas_w_b{j}.w;
-%                     b_old = capas_w_b{j}.b;
-%                     capas_w_b{j}.w = calculateNewW(w_old, p, e, alpha);
-%                     capas_w_b{j}.b = calculateNewBias(w_old, e, alpha);
-%                 end
-
 
 
 
